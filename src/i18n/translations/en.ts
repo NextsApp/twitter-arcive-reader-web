8b484e09dd0c5911484b7e6be4acaf2f5693dd9d
Ersin Keser <ersin@7style.net>
2019-10-05 11:28:25 +0200

    add translation i18next

---
+++ src/i18n/translations/en.ts
@@ -0,0 +1,7 @@
+export default {
+  translation: {
+    'File is read': 'File is read.',
+    'Data are store': 'File are Store.',
+    'File is being uploaded': 'File is being uploaded',
+  },
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import * as serviceWorker from './serviceWorker'
 
+import './i18n/i18n'
 import App from './app'
 import { Provider } from 'react-redux'
 import store from './app/store'
